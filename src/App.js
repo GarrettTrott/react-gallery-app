2aca8d70850aba5c9355b5f740233cded4d208bf
Garrett Trott <garretttrott@gmail.com>
2020-07-12 02:13:55 -0700

    adds components and boilerplates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React, { Component } from 'react'
+import Axios from 'axios'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import config.js from 'config.js'
 import './App.css'
 
-function App() {
-  return <div>Hello World</div>
+export default class App extends Component {
+  render() {
+    return <div>HEllo</div>
+  }
 }
-
-export default App
---
+++ src/components/Photo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Photo(props) {
+  return <div></div>
+}
+
+Photo.propTypes = {}
+
+export default Photo
