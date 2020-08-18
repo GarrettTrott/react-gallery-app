54df20f488008e430ee8dfea27476716fad02c2b
Garrett Trott <garretttrott@gmail.com>
2020-08-18 03:02:22 -0700

    add getNavData and state

--- src/App.js
+++ src/App.js
@@ -11,7 +11,12 @@ import Gallery from './components/Gallery'
 import NotFound from './components/NotFound'
 
 function App() {
-  const [data, setData] = useState([])
+  const [searchData, setSearchData] = useState([])
+  const [navData, setNavData] = useState({
+    skydiving: [],
+    drums: [],
+    blacklabs: [],
+  })
   const [isLoading, setIsLoading] = useState(true)
   const [query, setQuery] = useState()
 
@@ -19,34 +24,64 @@ function App() {
     setQuery(search)
   }
 
-  useEffect(() => {
+  const searchPhotos = async (topic) => {
     setIsLoading(true)
-    const fetchPhotos = async () => {
+    try {
+      const result = await axios(
+        `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${topic}&per_page=24&format=json&nojsoncallback=1`
+      )
+      await setSearchData(result.data.photos.photo)
+      setIsLoading(false)
+    } catch (err) {
+      console.log(new Error(err))
+      setIsLoading(false)
+    }
+  }
+
+  const getNavData = async () => {
+    setIsLoading(true)
+    for (let topic in navData) {
       try {
         const result = await axios(
-          `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
+          `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${topic}&per_page=24&format=json&nojsoncallback=1`
         )
-        setData(result.data.photos.photo)
-        setIsLoading(false)
+        setNavData({ ...navData, [topic]: result.data.photos.photo })
       } catch (err) {
         console.log(new Error(err))
-        setIsLoading(false)
       }
     }
-    fetchPhotos()
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    setIsLoading(true)
+    searchPhotos(query)
   }, [query])
+
+  useEffect(() => {
+    getNavData()
+  }, [])
 
   return (
     <Router>
       <div className="container">
         <SearchForm searchPictures={searchPictures} />
-        <Nav searchPictures={searchPictures} />
+        <Nav />
         {isLoading ? <h2>Loading...</h2> : null}
         <Switch>
-          <Route exact path="/" render={() => <Gallery data={data} />} />
-          <Route path="/skydiving" render={() => <Gallery data={data} />} />
-          <Route path="/drums" render={() => <Gallery data={data} />} />
-          <Route path="/blacklabs" render={() => <Gallery data={data} />} />
+          <Route exact path="/" render={() => <Gallery data={searchData} />} />
+          <Route
+            path="/skydiving"
+            render={() => <Gallery data={navData.skydiving} />}
+          />
+          <Route
+            path="/drums"
+            render={() => <Gallery data={navData.drums} />}
+          />
+          <Route
+            path="/blacklabs"
+            render={() => <Gallery data={navData.blacklabs} />}
+          />
           <Route component={NotFound} />
         </Switch>
       </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-function Nav({ searchPictures }) {
-  const selectPictures = (topic) => {
-    searchPictures(topic)
-  }
-
+function Nav() {
   return (
     <div>
       <nav className="main-nav">
         <ul>
           <li>
-            <NavLink to="/skydiving" isActive={selectPictures('Skydiving')}>
-              Skydiving
-            </NavLink>
+            <NavLink to="/skydiving">Skydiving</NavLink>
           </li>
           <li>
             <NavLink to="/drums">Drums</NavLink>
