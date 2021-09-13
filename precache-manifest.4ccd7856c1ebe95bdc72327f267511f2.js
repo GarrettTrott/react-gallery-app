a2f49a16a20359fee16d67a597a41cba76b58a16
Garrett Trott <garretttrott@gmail.com>
2021-09-12 22:07:54 -0700

    Updates

---
+++ precache-manifest.4ccd7856c1ebe95bdc72327f267511f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83f31eff32dfccd74bb2115757092f2d",
+    "url": "/GarrettTrott/react-gallery-app/index.html"
+  },
+  {
+    "revision": "bbba735028ace1ab46f8",
+    "url": "/GarrettTrott/react-gallery-app/static/css/main.532469c8.chunk.css"
+  },
+  {
+    "revision": "61dac58b88151e98954d",
+    "url": "/GarrettTrott/react-gallery-app/static/js/2.9892e185.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GarrettTrott/react-gallery-app/static/js/2.9892e185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbba735028ace1ab46f8",
+    "url": "/GarrettTrott/react-gallery-app/static/js/main.b310b1ba.chunk.js"
+  },
+  {
+    "revision": "f6fbac665530560fc180",
+    "url": "/GarrettTrott/react-gallery-app/static/js/runtime-main.8f51ff95.js"
+  }
+]);
